5e77e3cd5277e5f225f858276951013c3fd23a24
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-12 13:29:00 -0400

    Create main.java
    outline + init commit

---
+++ folder/main.java
@@ -0,0 +1,14 @@
+// librairy
+/*
+1- make a imprtable class that hold custom functions
+2- write func's 
+    - print
+    -printf
+    -sort
+3- make importable in vs
+4- make tostring with a dict of functions and uses
+5- test
+
+6- YYYYAAAAAAAAAAAYYYYYYYYYY
+
+*/
