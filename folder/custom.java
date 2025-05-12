8236101a11013b3c0a468f3756d53cb1f7eef575
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-12 13:41:16 -0400

    Create custom.java
    init importaible class

---
+++ folder/custom.java
@@ -0,0 +1,11 @@
+//static to use without object
+
+public class custom {
+    public static int square(int number) {
+        return number * number;
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
