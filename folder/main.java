1afbe08a0ef6f12062f5fe30a4c93ba31324c172
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-12 13:44:18 -0400

    Update main.java

--- folder/main.java
+++ folder/main.java
@@ -1,2 +1,13 @@
 //import
 
+public class Main 
+{
+  public static void main(String[] args)
+  {
+      int result = MyUtils.square(5);
+      String greeting = MyUtils.greet("Alice");
+
+      System.out.println("5 squared is: " + result);
+      System.out.println(greeting);
+  }
+}
