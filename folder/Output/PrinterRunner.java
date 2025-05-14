08f5ce891621418ec2330737b034759c9b768077
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-14 14:10:33 -0400

    Create PrinterRunner.java

---
+++ folder/Output/PrinterRunner.java
@@ -0,0 +1,8 @@
+public class PrinterRunner {
+    public static void main(String[] args) {
+        Printer.println();
+        Printer.println(3 + 7);
+        Printer.printf("hello %s!", marco);
+        Printer.print("\nbye!");
+    }
+}
