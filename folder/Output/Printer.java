2065ed8e3843faf794e81df0242675ee94aa320a
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-15 10:22:03 -0400

    Printer.java + info

--- folder/Output/Printer.java
+++ folder/Output/Printer.java
@@ -30,11 +30,76 @@ public class Printer {
         {
             result.append(arg);  // Convert each argument to string
         }
-        System.out.print("\t" + result.toString());
+        print("\t" + result.toString());
     }
 
     //printf                          P.S. : WHYYY IS IT SO EASY
     public static void printf(String format, Object... args) {
-        System.out.print(String.format(format, args));
+        print(String.format(format, args));
+    }
+
+    public static void getInfo()
+    {
+        println("____________________________________________________________________");
+        println("--Printer Class-- \n\t Custom short cuts for print related outputs.");
+        println("____________________________________________________________________");
+        
+        println("Methods:");
+        
+        printi("print(...);\n");
+        printi("Call: Printer.print();\n");
+        printi("\iWill print the same output as 'System.out.print()' would.\n");
+        printi("\iUsed as a short cut.\n\n");
+
+        printi("println(...);\n");
+        printi("Call: Printer.println();\n");
+        printi("\iWill print the same output as 'System.out.println()' would.\n");
+        printi("\iUsed as a short cut.\n\n");
+
+        printi("printi(...);\n");
+        printi("Call: Printer.printi();\n");
+        printi("\iWill print the same output as 'System.out.print()' but with\n");
+        printi("\ian added '\t' at the begining for indenting text.\n\n");
+
+        printi("printf(String, ...);\n");
+        printi("Call: Printer.printf();\n");
+        printi("\iWill print a formated string and working univeraly.\n");
+
+        printi("getInfo();\n");
+        printi("Call: Printer.getInfo();\n");
+        printi("\iWill print a decription of the Printer class and method definitions.\n");
+
+        println("____________________________________________________________________");
     }
 }
+
+/*
+____________________________________________________________________
+--Printer Class--
+    Custom short cuts for print related outputs
+____________________________________________________________________
+Methods:
+    print(...);
+    Call: Printer.print();
+        Will print the same output as 'System.out.print()' would.
+        Used as a short cut.
+        
+    println(...);
+    Call: Printer.println();
+        Will print the same output as 'System.out.println()' would.
+        Used as a short cut.
+        
+    printi(...);
+    Call: Printer.printi();
+        Will print the same output as 'System.out.print()' but with
+        an added '\t' at the begining for indenting text.
+
+    printf(String, ...);
+    Call: Printer.printf();
+        Will print a formated string and working univeraly.
+
+    getInfo();
+    Call: Printer.getInfo();
+        will print a decription of the Printer class and method definitions.
+____________________________________________________________________
+*/     
