25ca4b4ccb2c23fa634aa059f4d878a0d973d3a4
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-15 15:07:09 -0400

    Update Chance.java

--- folder/Chance/Chance.java
+++ folder/Chance/Chance.java
@@ -36,4 +36,70 @@ public class Chance {
         }
         return random.nextInt((max - min + 1)) + min;
     }
+
+    public static void getInfo()
+    {
+        println("____________________________________________________________________");
+        println("--Chance Class-- \n\t quick aceses probability functions");
+        println("____________________________________________________________________");
+        
+        println("Methods:");
+        
+        printi("roll(double);\n");
+        printi("Call: Chance.roll();\n");
+        printi("\ireturns boolean value of double% chance.\n");
+        printi("\iUsed as a short cut.\n\n");
+
+        printi("println(...);\n");
+        printi("Call: Printer.println();\n");
+        printi("\iWill print the same output as 'System.out.println()' would.\n");
+        printi("\iUsed as a short cut.\n\n");
+
+        printi("printi(...);\n");
+        printi("Call: Printer.printi();\n");
+        printi("\iWill print the same output as 'System.out.print()' but with\n");
+        printi("\ian added '\t' at the begining for indenting text.\n\n");
+
+        printi("printf(String, ...);\n");
+        printi("Call: Printer.printf();\n");
+        printi("\iWill print a formated string and working univeraly.\n");
+
+        printi("getInfo();\n");
+        printi("Call: Printer.getInfo();\n");
+        printi("\iWill print a decription of the Printer class and method definitions.\n");
+
+        println("____________________________________________________________________");
+    }
+}
+
+/*
+____________________________________________________________________
+--Printer Class--
+    Custom short cuts for print related outputs
+____________________________________________________________________
+Methods:
+    print(...);
+    Call: Printer.print();
+        Will print the same output as 'System.out.print()' would.
+        Used as a short cut.
+        
+    println(...);
+    Call: Printer.println();
+        Will print the same output as 'System.out.println()' would.
+        Used as a short cut.
+        
+    printi(...);
+    Call: Printer.printi();
+        Will print the same output as 'System.out.print()' but with
+        an added '\t' at the begining for indenting text.
+
+    printf(String, ...);
+    Call: Printer.printf();
+        Will print a formated string and working univeraly.
+
+    getInfo();
+    Call: Printer.getInfo();
+        will print a decription of the Printer class and method definitions.
+____________________________________________________________________
+*/     
 }
