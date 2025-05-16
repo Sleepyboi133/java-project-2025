7515f69886b3289197c53eddc6b5fdcd5632e72a
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-16 10:42:41 -0400

    Update Counter.java

--- folder/Counter/Counter.java
+++ folder/Counter/Counter.java
@@ -1,1 +1,32 @@
+class Counter {
+  public int count;
 
+  // init 0
+  public Counter() {
+    count = 0;
+  }
+  
+  // init with num
+  public Counter(int num) {
+    count = num;
+  }
+  // add 1
+  public void move() {
+    count++;
+  }
+
+  // add num
+  public void move(int num) {
+    count += count;
+  }
+
+  // set num
+  public void set(int num) {
+    count = num;
+  }
+  
+  // return
+  public int get() {
+    return count;
+  }
+}
