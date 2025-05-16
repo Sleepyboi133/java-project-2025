02e832c9d51b368fb2387d53d53bf466613c569a
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-16 13:35:07 -0400

    Update custom.java

--- src/utils/custom.java
+++ src/utils/custom.java
@@ -1,21 +1,4 @@
-//static to use without object//
-
-// librairy
-/*
-1- make a imprtable class that hold custom functions
-2- write func's 
-    - print
-    -printf
-    -sort
-3- make a packege version
-4- make importable in vs
-5- make tostring with a dict of functions and uses
-6- test
-
-7- YYYYAAAAAAAAAAAYYYYYYYYYY
-
-*/
-
+package utils;
 
 public class custom {
     public static int square(int number) {
