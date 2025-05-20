8447b4b3354823b8300c886e579ab42df565bed2
Sleepyboi133 <marco.a.rosenberg@gmail.com>
2025-05-20 13:06:07 -0400

    Update and rename main.java to Main.java

---
+++ myPackage/src/Main.java
@@ -0,0 +1,9 @@
+package scr; //if wanting to use main as package to import to work as the runner.
+
+import Package.*; // * will import all
+
+public class Main {
+    public static void main(String[] args) {
+        // 
+    }
+}
