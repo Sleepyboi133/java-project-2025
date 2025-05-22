f8188a541c376136b4012983c426b368211503fe
MarcoRosenberg <marco.a.rosenberg@gmail.com>
2025-05-22 13:07:02 -0400

    Update Main.java

--- myPackage/src/Main.java
+++ myPackage/src/Main.java
@@ -5,5 +5,28 @@ import Package.*; // * will import all
 public class Main {
     public static void main(String[] args) {
         Printer.println("hello world");
+
+//title
+//short decription
+//start user input loop
+        //prompt demo or info 
+        //after demo loop
+    //quit?
+
+//end program
+
+//demo
+//dice roller with random chance events
+        //take input of num and roll x dice
+        //random chance with bounus dice
+        //print a sorted list and output statistics 
+
+        //init as defalut or with args
+//
+
+
+//info class
+        // pull from all file info class and pring full list
+        
     }
 }
